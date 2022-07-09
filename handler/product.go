96514ee3039288a46f9bee3946a8f7e47a965b10
Gergo Gulyas <gulyas.gergo.karoly@gmail.com>
2022-07-09 12:37:48 +0200

    Price less than filter

--- handler/product.go
+++ handler/product.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gergogulyas/product-demo/product"
 )
@@ -18,6 +19,13 @@ func (h Product) List(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Query().Has("category") {
 		c := req.URL.Query().Get("category")
 		filters = append(filters, product.NewCategoryFilter(c))
+	}
+
+	if req.URL.Query().Has("priceLessThan") {
+		parsePrice, err := strconv.ParseInt(req.URL.Query().Get("priceLessThan"), 0, 32)
+		if err == nil {
+			filters = append(filters, product.NewPriceLessThanFilter(int(parsePrice)))
+		}
 	}
 
 	l := h.Repository.GetList(filters)
--- product/filter.go
+++ product/filter.go
@@ -39,3 +39,36 @@ func (f *CategoryFilter) ApplyOnList(list []Product) []Product {
 
 	return filtered
 }
+
+type PriceLessThanFilter struct {
+	price int
+}
+
+func NewPriceLessThanFilter(price int) *PriceLessThanFilter {
+	return &PriceLessThanFilter{price: price}
+}
+
+func (f *PriceLessThanFilter) condition() func(p Product) bool {
+	return func(p Product) bool {
+		return p.Price <= f.price
+	}
+}
+
+func (f *PriceLessThanFilter) ApplyOnItem(product Product) bool {
+	valid := f.condition()
+
+	return valid(product)
+}
+
+func (f *PriceLessThanFilter) ApplyOnList(list []Product) []Product {
+	filtered := []Product{}
+	valid := f.condition()
+
+	for _, product := range list {
+		if valid(product) {
+			filtered = append(filtered, product)
+		}
+	}
+
+	return filtered
+}
