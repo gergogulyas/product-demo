230f6285fc4a774f532dfd8133dfd4f36836e32c
Gergo Gulyas <gulyas.gergo.karoly@gmail.com>
2022-07-10 23:06:01 +0200

    Update discount

--- product/discount.go
+++ product/discount.go
@@ -2,46 +2,60 @@ package product
 
 import "strings"
 
-type DiscountRepository struct {
-	Discounts []Discounter
-}
-
-func (r *DiscountRepository) init() {
-	skuDiscount := SKUDiscount{SKU: "000003", Percent: 15}
-	categoryDiscount := CategoryDiscount{Category: "boots", Percent: 30}
-
-	r.Discounts = append(r.Discounts, skuDiscount, categoryDiscount)
-}
-
-func NewDiscountRepository() *DiscountRepository {
-	discountR := &DiscountRepository{}
-	discountR.init()
-
-	return discountR
-}
-
 type Discounter interface {
 	Apply(*Product)
 }
 
+type DiscountRepository struct {
+	Discounts []Discounter
+}
+
+func NewDiscountRepository() *DiscountRepository {
+	repository := &DiscountRepository{}
+	repository.init()
+
+	return repository
+}
+
+func (r *DiscountRepository) init() {
+	skuDiscount := NewSKUDiscount("000003", 15)
+	categoryDiscount := NewCategoryDiscount("boots", 30)
+
+	r.Discounts = append(r.Discounts, skuDiscount, categoryDiscount)
+}
+
+func NewCategoryDiscount(category string, percent int) CategoryDiscount {
+	return CategoryDiscount{
+		category: category,
+		percent:  percent,
+	}
+}
+
 type CategoryDiscount struct {
-	Category string
-	Percent  int
+	category string
+	percent  int
 }
 
 func (cd CategoryDiscount) Apply(product *Product) {
-	if strings.EqualFold(product.Category, cd.Category) && product.Discount() < cd.Percent {
-		product.SetDiscount(cd.Percent)
+	if strings.EqualFold(product.Category, cd.category) && product.Discount() < cd.percent {
+		product.SetDiscount(cd.percent)
+	}
+}
+
+func NewSKUDiscount(sku string, percent int) SKUDiscount {
+	return SKUDiscount{
+		sku:     sku,
+		percent: percent,
 	}
 }
 
 type SKUDiscount struct {
-	SKU     string
-	Percent int
+	sku     string
+	percent int
 }
 
 func (sd SKUDiscount) Apply(product *Product) {
-	if strings.EqualFold(product.SKU, sd.SKU) && product.Discount() < sd.Percent {
-		product.SetDiscount(sd.Percent)
+	if strings.EqualFold(product.SKU, sd.sku) && product.Discount() < sd.percent {
+		product.SetDiscount(sd.percent)
 	}
 }
